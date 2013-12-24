349e316321ef92c3855131a3320f2b8e7bef5d10
sanford78 <sanford78@gmail.com>
2013-12-24 01:42:42 -0500

    Building out Universal Header include
    made the universal header work with  call to action buttons and colors
    set in the wp backend

--- wp-content/themes/boilerplate/internal_header.php
+++ wp-content/themes/boilerplate/internal_header.php
@@ -1,8 +1,11 @@
   <div id="internal-header-wrap" style="width:<?php the_field('heading_text_width'); ?>px">
     <div id="internal-header" class="centered" style="background:url(<?php the_field('page_heading_image'); ?>); background-repeat: no-repeat; background-position:right;">
       <header id="internal-header-text">
-        <h1><?php the_field('page_heading_text'); ?></h1>
-        <p><?php the_field('page_subheading_text'); ?></p>
+        <h1 style="color: <?php the_field('heading_text_color');?>;"><?php the_field('page_heading_text'); ?></h1>
+        <p style="color: <?php the_field('subheading_text_color');?>;"><?php the_field('page_subheading_text'); ?></p>
+        <?php if(get_field('include_heading_call_to_action')){ ?>
+         <a href="<?php the_field('page_heading_call_to_action_link'); ?>"><span class="lighter-grey-button"><?php the_field('page_heading_call_to_action'); ?></span></a>
+        <?php } ?>
       </header>
     </div>
   </div>
