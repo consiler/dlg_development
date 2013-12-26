dd6db9df2ab6b34fdd2e5e246d6587293784dffc
MP <consiler@gmail.com>
2013-12-25 22:56:59 -0600

    changing slider

--- wp-content/themes/boilerplate/js/slider.js
+++ wp-content/themes/boilerplate/js/slider.js
@@ -1,17 +1,27 @@
 jQuery(document).ready(function() {
-  //var lastSlide = $('.orbit-slides-container li:last-child');
+  $(".example-orbit").on("ready.fndtn.orbit", function(event) {
+    $(".orbit-prev").hide();
+  });
   jQuery(document).foundation({
     orbit: {
       animation: 'fade',
-      timer_speed: 1000,
+      timer_speed: 2000,
       animation_speed: 500,
       slide_number: false,
-      bullets: false,
+      bullets: true,
       pause_on_hover: false,
       next_on_click: false,
       after_slide_change: function() {
         if($('.orbit-slides-container li:last-child').hasClass('active')) {
+          $(".orbit-next").fadeOut(100);
           $(".orbit-timer").click();
+        } else {
+          $(".orbit-next").fadeIn(100);
+        }
+        if($('.orbit-slides-container li:first-child').hasClass('active')) {
+          $(".orbit-prev").fadeOut(100);
+        } else {
+          $(".orbit-prev").fadeIn(100);
         }
       }
     }
