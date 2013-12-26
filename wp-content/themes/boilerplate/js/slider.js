f3e41e333436d8f327500132d4972d138267b04e
MP <consiler@gmail.com>
2013-12-25 22:58:28 -0600

    slider tweak

--- wp-content/themes/boilerplate/js/slider.js
+++ wp-content/themes/boilerplate/js/slider.js
@@ -13,15 +13,15 @@ jQuery(document).ready(function() {
       next_on_click: false,
       after_slide_change: function() {
         if($('.orbit-slides-container li:last-child').hasClass('active')) {
-          $(".orbit-next").fadeOut(100);
+          $(".orbit-next").fadeOut(150);
           $(".orbit-timer").click();
         } else {
-          $(".orbit-next").fadeIn(100);
+          $(".orbit-next").fadeIn(200);
         }
         if($('.orbit-slides-container li:first-child').hasClass('active')) {
-          $(".orbit-prev").fadeOut(100);
+          $(".orbit-prev").fadeOut(200);
         } else {
-          $(".orbit-prev").fadeIn(100);
+          $(".orbit-prev").fadeIn(200);
         }
       }
     }
