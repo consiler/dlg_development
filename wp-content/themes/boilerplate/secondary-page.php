2353305b1f9493673622c5039f694a91b7ba9b86
sanford78 <sanford78@gmail.com>
2013-12-26 03:08:34 -0500

    Updating secondary page

--- wp-content/themes/boilerplate/secondary-page.php
+++ wp-content/themes/boilerplate/secondary-page.php
@@ -18,11 +18,17 @@ get_header(); ?>
   <!-- Internal Header -->
   <?php include 'large_internal_header.php';?>
   
-  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
   <div id="internal-lower-wrap">
     <div id="internal-lower" class="centered">
       <div id="internal-main">
+
+        <!-- Three Columns -->
+        <?php if (get_field('include_three_column')){ ?>
+        <div>Include three cols here</div>
+        <?php } ?>
+
         <article id="internal-main-content" class="secondary">
+          <!-- Main Content -->
           <?php the_content(get_the_ID()); ?>
         </article>
       </div>
