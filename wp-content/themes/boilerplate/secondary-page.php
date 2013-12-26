60881a2233279f8d1fb737faacab436935ddb9ae
sanford78 <sanford78@gmail.com>
2013-12-26 04:00:02 -0500

    Three cols added to secondary page

--- wp-content/themes/boilerplate/secondary-page.php
+++ wp-content/themes/boilerplate/secondary-page.php
@@ -23,7 +23,38 @@ get_header(); ?>
     <div id="internal-lower" class="centered">
       <!-- Three Columns -->
         <?php if (get_field('include_three_column')){ ?>
-        <div>Include three cols here</div>
+          <div id="internal-lower-wrap">
+    <div id="fp-testimonials" class="centered">
+    <div id="fp-testimonials-seemore">
+      <?php
+      $header = get_field('header_1');
+      $copy = get_field('copy_1');
+      ?>
+      <h4><?php echo $header ?></h4>
+      <p><?php echo $copy ?></p>
+    </div>
+    <div id="fp-testimonials-seemore">
+      <?php
+      $header = get_field('header_2');
+      $copy = get_field('copy_2');
+      $cta = get_field('cta_2');
+      $cta_link = get_field('cta_link_2');
+      ?>
+      <h4><?php echo $header ?></h4>
+      <p><?php echo $copy ?></p>
+      <a href="<?php echo $cta_link ?>"><span class="light-grey-button"><?php echo $cta ?></span></a>
+    </div>
+    <div id="fp-testimonials-seemore">
+      <?php
+      $header = get_field('header_3');
+      $copy = get_field('copy_3');
+      $cta = get_field('cta_3');
+      $cta_link = get_field('cta_link_3');
+      ?>
+      <h4><?php echo $header ?></h4>
+      <p><?php echo $copy ?></p>
+      <a href="<?php echo $cta_link ?>"><span class="light-grey-button"><?php echo $cta ?></span></a>
+    </div>
         <?php } ?>
       <div id="internal-main"<?php if(!get_field('has_sidebar')){ ?> class="internal-main-fullwidth"<?php } ?>>
         <nav id="internal-main-nav">
@@ -48,7 +79,7 @@ get_header(); ?>
       </div>
       <?php if(get_field('has_sidebar')){ ?>
       <aside id="internal-sidebar">
-        <?php
+      <?php
         $rspt = get_field('right_sidebar_promo_tile');
         if($rspt){ ?>
           <div id="internal-sidebar-promo-tile">
