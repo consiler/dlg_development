a7c06283134f883dbb97f86f0df68d184397851e
sanford78 <sanford78@gmail.com>
2013-12-27 01:01:19 -0500

    Updating smaller internal header

--- wp-content/themes/boilerplate/internal_header.php
+++ wp-content/themes/boilerplate/internal_header.php
@@ -1,8 +1,8 @@
   <div id="internal-header-wrap">
     <div id="internal-header" class="centered" style="background:url(<?php the_field('page_heading_image'); ?>); background-repeat: no-repeat; background-position:right;">
       <header id="internal-header-text" style="width:<?php the_field('heading_text_width'); ?>px;">
-        <h1 style="color: <?php the_field('heading_text_color');?>;"><?php the_field('page_heading_text'); ?></h1>
-        <p style="color: <?php the_field('subheading_text_color');?>;"><?php the_field('page_subheading_text'); ?></p>
+        <h1 style="color: <?php the_field('heading_text_color');?>; width:<?php the_field('heading_text_width'); ?>px;"><?php the_field('page_heading_text'); ?></h1>
+        <p style="color: <?php the_field('subheading_text_color');?>; width:<?php the_field('subheading_text_width'); ?>px;"><?php the_field('page_subheading_text'); ?></p>
         <?php if(get_field('include_heading_call_to_action')){ ?>
          <a href="<?php the_field('page_heading_call_to_action_link'); ?>"><span class="lighter-grey-button"><?php the_field('page_heading_call_to_action'); ?></span></a>
         <?php } ?>
